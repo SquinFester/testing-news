e4b9fd18ebf844f2e8d7be39921226978e6430e6
squinfester <squinfester@gmail.com>
2023-05-29 14:41:00 +0200

    first time with react with ts it is wierd or tutorial which i am watching sucks

--- TS/lesson2-react-ts-vite/src/App.tsx
+++ TS/lesson2-react-ts-vite/src/App.tsx
@@ -3,9 +3,11 @@ import { useState } from "react";
 import Section from "./components/Section";
 import Counter from "./components/Counter";
 import Heading from "./components/Heading";
+import List from "./components/List";
 
 function App() {
   const [count, setCount] = useState<number>(2);
+  const items: string[] = ["Item1", "Item2", "Item3"];
 
   return (
     <main className="text-white grid justify-center">
@@ -13,6 +15,7 @@ function App() {
       <Section>
         <Counter setCount={setCount}>Count is {count}</Counter>
       </Section>
+      <List items={items} render={(item: string) => <span>{item}</span>} />
     </main>
   );
 }
