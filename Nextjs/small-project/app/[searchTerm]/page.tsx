c6b92ce4f2267848f4a77df65f2b93181b4fa115
SquinFester <bartoszprzymencki@gmail.com>
2023-06-13 22:45:04 +0200

    finish simple example of next js fetching data

--- Nextjs/small-project/app/[searchTerm]/page.tsx
+++ Nextjs/small-project/app/[searchTerm]/page.tsx
@@ -1,6 +1,7 @@
-import type { Metadata } from "next";
-
 import getWikiResults from "@/lib/getWikiResults";
+import Item from "./components/Item";
+import { Suspense } from "react";
+import Loading from "./loading";
 
 type Props = {
   params: {
@@ -32,12 +33,14 @@ export default async function SearchResults({ params: { searchTerm } }: Props) {
   const content = (
     <main className="bg-slate-200 mx-auto max-w-lg py-1 min-h-screen">
       {results ? (
-        Object.values(results).map((result) => <p>{JSON.stringify(result)}</p>)
+        Object.values(results).map((result) => (
+          <Item key={result.pageid} result={result} />
+        ))
       ) : (
         <p>{`${searchTerm} not found`}</p>
       )}
     </main>
   );
 
-  return content;
+  return <Suspense fallback={<Loading />}>{content}</Suspense>;
 }
