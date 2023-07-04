c06ba55ea80ea8fb46e33a5929e25626a95ce544
bprzymenckideveloart <137422584+bprzymenckideveloart@users.noreply.github.com>
2023-07-04 15:50:50 +0200

    first step and ofc not working xD

---
+++ ReactNative/first-step/components/Categories.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+const Categories = () => {
+  return (
+    <View>
+      <Text>Categories</Text>
+    </View>
+  );
+};
+export default Categories;
---
+++ ReactNative/first-step/screens/HomeScreen.tsx
@@ -0,0 +1,124 @@
+import { useNavigation } from "@react-navigation/native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TextInput,
+  StatusBar,
+  ScrollView,
+} from "react-native";
+import { useLayoutEffect } from "react";
+import {
+  UserIcon,
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsVerticalIcon,
+} from "react-native-heroicons/outline";
+import Categories from "../components/Categories";
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
+  return (
+    <SafeAreaView
+      style={{
+        backgroundColor: "#fff",
+        flex: 1,
+      }}
+    >
+      <StatusBar backgroundColor="#000" />
+      <View
+        style={{
+          paddingBottom: 15,
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          gap: 10,
+          paddingHorizontal: 15,
+        }}
+      >
+        <Image
+          source={{
+            uri: "https://links.papareact.com/wru",
+          }}
+          style={{
+            height: 30,
+            width: 30,
+            backgroundColor: "lightgray",
+            borderRadius: 9999,
+          }}
+        />
+        <View
+          style={{
+            flex: 1,
+          }}
+        >
+          <Text
+            style={{
+              fontWeight: "bold",
+              color: "lightgray",
+              fontSize: 12,
+            }}
+          >
+            Deliver Now!
+          </Text>
+          <Text
+            style={{
+              fontSize: 20,
+              fontWeight: "bold",
+            }}
+          >
+            Current Location
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
+        </View>
+        <UserIcon size={35} color="#00CCBB" />
+      </View>
+
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          paddingBottom: 10,
+          paddingHorizontal: 15,
+        }}
+      >
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 5,
+            backgroundColor: "#e5e7eb",
+            padding: 5,
+            flex: 1,
+          }}
+        >
+          <MagnifyingGlassIcon size={20} color="gray" />
+          <TextInput
+            placeholder="Search..."
+            keyboardType="default"
+            style={{
+              width: "100%",
+            }}
+          />
+        </View>
+        <AdjustmentsVerticalIcon color="#00CCBB" />
+      </View>
+
+      <ScrollView>
+        <Categories />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+export default HomeScreen;
