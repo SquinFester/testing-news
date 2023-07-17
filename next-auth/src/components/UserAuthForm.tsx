8169ece22901e397fece3022f524b988f669e8a3
bprzymenckideveloart <bartoszprzymencki@gmail.com>
2023-07-17 14:16:55 +0200

    create toaster

--- next-auth/src/components/UserAuthForm.tsx
+++ next-auth/src/components/UserAuthForm.tsx
@@ -5,23 +5,26 @@ import { signIn } from "next-auth/react";
 import { Button } from "./ui/Button";
 import { cn } from "@/lib/utils";
 import { Icons } from "./Icons";
+import { useToast } from "@/hooks/use-toast";
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export const UserAuthForm = ({ className, ...props }: UserAuthFormProps) => {
   const [isLoading, setIsLoading] = useState(false);
+  const { toast } = useToast();
 
   const loginWithGoogle = async () => {
     setIsLoading(true);
 
     try {
+      throw new Error();
       await signIn("google");
     } catch (error) {
-      //   toast({
-      //     title: "Error",
-      //     description: "There was an error logging in with Google",
-      //     variant: "destructive",
-      //   });
+      toast({
+        title: "Error",
+        description: "There was an error logging in with Google",
+        variant: "destructive",
+      });
     } finally {
       setIsLoading(false);
     }
