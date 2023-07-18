2f721a2e5cc9617f83fe3a4bb2afa4eec6a022eb
bprzymenckideveloart <bartoszprzymencki@gmail.com>
2023-07-18 19:54:29 +0200

    make sign up page

--- next-auth/src/components/CloseModal.tsx
+++ next-auth/src/components/CloseModal.tsx
@@ -1,12 +1,19 @@
+"use client";
+
 import { X } from "lucide-react";
 import { Button } from "./ui/Button";
+import { useRouter } from "next/navigation";
 
 export const CloseModal = () => {
+  const router = useRouter();
+
   return (
     <Button
       variant="subtle"
       className="h-6 w-6 p-0 rounded-md"
-      aria-label="close modal"
+      onClick={() => {
+        router.back();
+      }}
     >
       <X className="w-4 h-4" />
     </Button>
