b15018cf89f11fdc113ffaefe253023e74ee41e9
bprzymenckideveloart <bartoszprzymencki@gmail.com>
2023-07-19 13:43:23 +0200

    make joining to post

---
+++ next-auth/src/components/SubscribeLeaveToggle.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { useMutation } from "@tanstack/react-query";
+import { Button } from "./ui/Button";
+import { SubscribeToSubredditPayload } from "@/lib/validators/subreddit";
+import axios, { AxiosError } from "axios";
+import { useCustomTost } from "@/hooks/use-custom-toast";
+import { toast } from "@/hooks/use-toast";
+import { startTransition } from "react";
+import { useRouter } from "next/navigation";
+
+export const SubscribeLeaveToggle = ({
+  subredditId,
+  subredditName,
+}: {
+  subredditId: string;
+  subredditName: string;
+}) => {
+  const isSubscribed = false;
+  const { loginToast } = useCustomTost();
+  const router = useRouter();
+
+  const { mutate } = useMutation({
+    mutationFn: async () => {
+      const payload: SubscribeToSubredditPayload = {
+        subredditId,
+      };
+      const { data } = await axios.post("/api/subreddit/subscribe", payload);
+      return data as string;
+    },
+    onError: (err) => {
+      if (err instanceof AxiosError) {
+        if (err.response?.status == 401) {
+          return loginToast();
+        }
+      }
+
+      return toast({
+        title: "There was a problem",
+        description: "Something went wrong, please try again",
+        variant: "destructive",
+      });
+    },
+    onSuccess: () => {
+      startTransition(() => {
+        router.refresh();
+      });
+      return toast({
+        title: "Subscribed",
+        description: `You are now subscribed to r/${subredditName}`,
+      });
+    },
+  });
+
+  return isSubscribed ? (
+    <Button className="w-full mt-1 mb-4">Leave community</Button>
+  ) : (
+    <Button className="w-full mt-1 mb-4">Join to post</Button>
+  );
+};
